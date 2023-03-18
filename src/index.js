65028b105b2b19adfaa7ec65c6ea39e7d0281aac
vijai <vijai.panneer@kone.com>
2023-03-18 16:15:22 +0200

    add socket logic and healthcheck endpoint

---
+++ src/index.js
@@ -0,0 +1,41 @@
+const express = require("express")
+const app = express()
+const cors = require("cors")
+const http = require('http').Server(app);
+const PORT = 4000
+const socketIO = require('socket.io')(http, {
+    cors: {
+        origin: "http://localhost:3000"
+    }
+});
+
+app.use(cors())
+let users = []
+
+socketIO.on('connection', (socket) => {
+    console.log(`⚡: ${socket.id} user just connected!`)  
+    socket.on("message", data => {
+      socketIO.emit("messageResponse", data)
+    })
+
+    socket.on("newUser", data => {
+      users.push(data)
+      socketIO.emit("newUserResponse", users)
+    })
+ 
+    socket.on('disconnect', () => {
+      console.log('🔥: A user disconnected');
+      users = users.filter(user => user.socketID !== socket.id)
+      socketIO.emit("newUserResponse", users)
+      socket.disconnect()
+    });
+});
+
+app.get("/health-check", (req, res) => {
+  res.status(200).json({message: "Hello"})
+});
+
+   
+http.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+});
