0f0e0e2c965a4647a13ce13c6bf36551b36d9ce9
vijai <vijai.panneer@kone.com>
2023-03-18 19:16:57 +0200

    create login page

---
+++ src/components/ChatPage.js
@@ -0,0 +1,7 @@
+import React, { useEffect, useState, useRef} from 'react'
+
+
+const ChatPage = () => { 
+}
+
+export default ChatPage
---
+++ src/components/Login.js
@@ -0,0 +1,48 @@
+import React, {useState} from 'react'
+import {useNavigate} from "react-router-dom"
+import socketIO from "socket.io-client"
+
+const Login = () => {
+    const navigate = useNavigate()
+    const [userName, setUserName] = useState("")
+    const [chatServerAddress, setChatServerAddress] = useState("")
+    
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const socket = socketIO.connect(chatServerAddress)
+        localStorage.setItem("userName", userName)
+        localStorage.setItem("socket", socket)
+        try {
+          socket.emit("newUser", {userName, socketID: socket.id})
+          navigate("/chat")
+        } catch (err) {
+          console.log(err);
+        }
+    }
+  return (
+    <form className='home__container' onSubmit={handleSubmit}>
+        <h2 className='home__header'>Sign in to Open Chat</h2>
+        <label htmlFor="username">Username</label>
+        <input type="text" 
+        minLength={10} 
+        name="username" 
+        id='username'
+        className='username__input' 
+        value={userName} 
+        onChange={e => setUserName(e.target.value)}
+        />
+        <label htmlFor="server">Chat Server address</label>
+        <input type="text" 
+        minLength={10} 
+        name="chatServerAddress" 
+        id='chatServerAddress'
+        className='username__input' 
+        value={chatServerAddress} 
+        onChange={e => setChatServerAddress(e.target.value)}
+        />
+        <button className='home__cta'>Login</button>
+    </form>
+  )
+}
+
+export default Login
