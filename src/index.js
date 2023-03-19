03652d4ca6fb34dca07c7ffb2b1f1a1df9ccbdcb
vijai <vijai.panneer@kone.com>
2023-03-19 09:01:44 +0200

    add typing endpoint

--- src/index.js
+++ src/index.js
@@ -18,6 +18,10 @@ socketIO.on('connection', (socket) => {
       socketIO.emit("messageResponse", data)
     })
 
+    socket.on("typing", data => (
+      socket.broadcast.emit("typingResponse", data)
+    ))
+
     socket.on("newUser", data => {
       users.push(data)
       socketIO.emit("newUserResponse", users)
