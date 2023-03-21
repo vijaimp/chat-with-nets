d5d679ad4c43e1242288ac4faa471df7968d57cc
vijai <vijai.panneer@kone.com>
2023-03-21 21:14:54 +0200

    refactor

--- src/components/ChatBody.js
+++ src/components/ChatBody.js
@@ -24,23 +24,14 @@ const ChatBody = ({ socket, messages, lastMessageRef }) => {
         </button>
       </header>
       <div className='message__container'>
-        {messages.map((message) =>
-          message.name === localStorage.getItem("userName") ? (
-            <div className='message__chats' key={message.id}>
-              <p className='sender__name'>You</p>
-              <div className='message__sender'>
-                <p>{message.text}</p>
-              </div>
+        {messages.map((message) => (
+          <div className='message__chats' key={message.id}>
+            <p>{message.name}</p>
+            <div className='message__recipient'>
+              <p>{message.text}</p>
             </div>
-          ) : (
-            <div className='message__chats' key={message.id}>
-              <p>{message.name}</p>
-              <div className='message__recipient'>
-                <p>{message.text}</p>
-              </div>
-            </div>
-          )
-        )}
+          </div>
+        ))}
         <div ref={lastMessageRef} />
       </div>
     </>
--- src/components/Login.js
+++ src/components/Login.js
@@ -42,6 +42,16 @@ const Login = ({ getSocket }) => {
   return (
     <form className='home__container' onSubmit={handleSubmit}>
       <h2 className='home__header'>Sign in to Open Chat</h2>
+      <label htmlFor='server'>Chat Server address</label>
+      <input
+        type='text'
+        minLength={10}
+        name='chatServerAddress'
+        id='chatServerAddress'
+        className='chat_address__input'
+        value={chatServerAddress}
+        onChange={(e) => setChatServerAddress(e.target.value)}
+      />
       <label htmlFor='username'>Username</label>
       <input
         type='text'
@@ -52,17 +62,7 @@ const Login = ({ getSocket }) => {
         value={userName}
         onChange={(e) => setUserName(e.target.value)}
       />
-      <label htmlFor='server'>Chat Server address</label>
-      <input
-        type='text'
-        minLength={10}
-        name='chatServerAddress'
-        id='chatServerAddress'
-        className='username__input'
-        value={chatServerAddress}
-        onChange={(e) => setChatServerAddress(e.target.value)}
-      />
-      <button className='home__cta'>Login</button>
+      <button className='home__cta'>Submit</button>
     </form>
   );
 };
